a23a327633ae0adda34323f4f6aad58569d0952d
G3Kaliaperumal <kaliaperumalgayathri27@gmail.com>
2020-06-11 01:23:54 +0530

    Update firstGUIProgram.py - PEP8 Standards

--- Basics/firstGUIProgram.py
+++ Basics/firstGUIProgram.py
@@ -1,21 +1,20 @@
-#Exercise!
+# Exercise!
 # Display the image below to the right hand side where
 # the 0 is going to be ' ', and the 1 is going to be '*'.
 # This will reveal an image!
 picture = [
-  [0,0,0,1,0,0,0],
-  [0,0,1,1,1,0,0],
-  [0,1,1,1,1,1,0],
-  [1,1,1,1,1,1,1],
-  [0,0,0,1,0,0,0],
-  [0,0,0,1,0,0,0]
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 1, 1, 1, 0, 0],
+    [0, 1, 1, 1, 1, 1, 0],
+    [1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0]
 ]
 
 for tempList in picture:
-  for item in tempList:
-    if item == 0:
-      print(' ', end='')
-    elif item == 1:
-      print('*', end='')
-  print()
-  
+    for item in tempList:
+        if item == 0:
+            print(' ', end='')
+        elif item == 1:
+            print('*', end='')
+    print()
