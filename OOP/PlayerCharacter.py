d8cca97b57572a918c022143dfc376a148098d55
G3Kaliaperumal <kaliaperumalgayathri27@gmail.com>
2020-06-13 16:32:47 +0530

    Lecture 126 - super()

--- OOP/PlayerCharacter.py
+++ OOP/PlayerCharacter.py
@@ -1,10 +1,25 @@
 class PlayerCharacter:
     def __init__(self, name):
-        self.name = name
+        self._name = name
 
     def getName(self):
-        return(self.name)
+        return(self._name)
+
+
+class HK47(PlayerCharacter):
+    def __init__(self, name, game):
+        super().__init__(name)
+        self._game = game
+
+    def getGame(self):
+        return(self._game)
+
+    def printInfo(self):
+        print(f'{super().getName()} got introduced in the game {self._game}')
 
 
 player1 = PlayerCharacter('Christopher')
 print(player1.getName())
+
+killer_bot = HK47('Killerbot', 'Star Wars: Knights of the Old Republic')
+killer_bot.printInfo()
