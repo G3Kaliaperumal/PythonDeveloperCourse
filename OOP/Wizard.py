a9c97d7fd6c94e6b8e49136cbda8f080622380e4
G3Kaliaperumal <kaliaperumalgayathri27@nielsen.com>
2020-06-13 14:47:42 +0530

    Lecture 124 - Polymorphism

--- OOP/Wizard.py
+++ OOP/Wizard.py
@@ -9,10 +9,27 @@ class Wizard(User):
         self._name = name
         self._power = power
 
+    # Polymorphism
     def showMyPower(self):
-        print(f'I am {self._name} and my power is {self._power}')
+        print(
+            f'I am a wizard. My name is {self._name} and my power is {self._power}')
+
+
+class Archer(User):
+    def __init__(self, name, power):
+        self._name = name
+        self._power = power
+
+    # Polymorphism
+    def showMyPower(self):
+        print(
+            f'I am an archer. My name is {self._name} and my power is {self._power}')
 
 
 hermoine = Wizard('Hermoine', 'lifting things using my mind')
 hermoine.sign_in()
 hermoine.showMyPower()
+
+robin = Archer('Robin', 'a perfect shot. My aim never misses!')
+robin.sign_in()
+robin.showMyPower()
