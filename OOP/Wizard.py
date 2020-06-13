bc4c67e7c247ef9a14c69a4960b4ace154078450
G3Kaliaperumal <kaliaperumalgayathri27@gmail.com>
2020-06-13 13:56:58 +0530

    Create Wizard.py

---
+++ OOP/Wizard.py
@@ -0,0 +1,17 @@
+class User:
+    def sign_in(self):
+        print('logged in')
+
+
+class Wizard(User):
+    def __init__(self, name, power):
+        self._name = name
+        self._power = power
+
+    def showMyPower(self):
+        print(f'I am {self._name} and my power is {self._power}')
+
+
+hermoine = Wizard('Hermoine', 'lifting things using my mind')
+hermoine.sign_in()
+hermoine.showMyPower()
