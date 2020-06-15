1dd906917b5c11bba2e9183e4bb7e041f060d358
G3Kaliaperumal <kaliaperumalgayathri27@gmail.com>
2020-06-15 21:45:26 +0530

    Lecture 136 - filter()

--- FP/PopularMethods.py
+++ FP/PopularMethods.py
@@ -1,6 +1,15 @@
+my_list = [1, 2, 3, 4, 5]
+
+
 # Map function
 def multiply_by_2(item):
     return item * 2
 
 
-print(list(map(multiply_by_2, [1, 2, 3])))
+# Filter function
+def find_odd_numbers(item):
+    return item % 2 != 0
+
+
+print(list(map(multiply_by_2, my_list)))
+print(list(filter(find_odd_numbers, my_list)))
