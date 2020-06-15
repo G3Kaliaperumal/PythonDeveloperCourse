88cc069e925a94d0307bf671b77c2c2438e33351
G3Kaliaperumal <kaliaperumalgayathri27@gmail.com>
2020-06-15 22:01:29 +0530

    Lecture 138 - reduce()

--- FP/PopularMethods.py
+++ FP/PopularMethods.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 ones = [1, 2, 3, 4, 5]
 tens = [10, 20, 30, 40, 50]
 hundreds = [100, 200, 300, 400, 500]
@@ -13,7 +15,13 @@ def find_odd_numbers(item):
     return item % 2 != 0
 
 
+# Reduce function
+def multiplier(result, initial_value):
+    return result * initial_value
+
+
 print(list(map(multiply_by_2, ones)))
 print(list(filter(find_odd_numbers, ones)))
 # Zip function
 print(list(zip(ones, tens, hundreds)))
+print(reduce(multiplier, ones, 1))  # => 1 * 2 * 3 * 4 * 5 = 120
