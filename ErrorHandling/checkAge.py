85bb514d0870a9277f2bac0660f698a3c98f981e
G3Kaliaperumal <kaliaperumalgayathri27@gmail.com>
2020-06-16 22:41:18 +0530

    Lecture 154: Handling multiple errors

--- ErrorHandling/checkAge.py
+++ ErrorHandling/checkAge.py
@@ -2,10 +2,10 @@ while True:
     try:
         age = int(input('What is your age?'))
         100 / age
-    except ValueError:
+    except (ValueError, ZeroDivisionError):
         print('Please enter a number')
     except:
-        print('Please enter a valid number(Not 0 please)')
+        print('Some runtime exception has occurred :(')
     else:
         print(f'Your age is {age}')
         break
