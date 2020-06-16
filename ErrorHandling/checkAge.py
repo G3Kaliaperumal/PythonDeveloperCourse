1a6ca6800215ed4e52a02489f122335926f8dbd8
G3Kaliaperumal <kaliaperumalgayathri27@gmail.com>
2020-06-16 23:05:35 +0530

    Lecture 156: Throw exceptions and finally

--- ErrorHandling/checkAge.py
+++ ErrorHandling/checkAge.py
@@ -2,6 +2,7 @@ while True:
     try:
         age = int(input('What is your age?'))
         100 / age
+        raise TypeError('Oops')
     except (ValueError, ZeroDivisionError):
         print('Please enter a number')
     except:
@@ -9,3 +10,5 @@ while True:
     else:
         print(f'Your age is {age}')
         break
+    finally:
+        print('I am always watching you...')
