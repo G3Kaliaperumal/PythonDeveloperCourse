5da7452d5e41e8450d7f90be956013ce726166b3
G3Kaliaperumal <kaliaperumalgayathri27@gmail.com>
2020-06-18 22:45:24 +0530

    Lecture 167: __name__ and __main__

--- Advanced/Modules/main.py
+++
@@ -1,3 +0,0 @@
-from shopping.shopping_cart import buy
-
-print(buy('apple'))
---
+++ Advanced/Modules/main_file.py
@@ -0,0 +1,6 @@
+from shopping.shopping_cart import buy
+
+print(buy('apple'))
+
+if __name__ == '__main__':
+    print('File that is being run is internally called __main__')
