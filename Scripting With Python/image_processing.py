7ea2382fe3476ab66e9337947d15b0ddef5f3f9e
G3Kaliaperumal <kaliaperumalgayathri27@gmail.com>
2020-06-22 22:34:03 +0530

    Lecture 206: Images with Python 2

--- Scripting With Python/image_processing.py
+++ Scripting With Python/image_processing.py
@@ -3,3 +3,17 @@ from PIL import Image, ImageFilter
 image = Image.open('images\\original.jpg')
 filtered_image = image.filter(ImageFilter.BLUR)
 filtered_image.save('images\\Blurred.jpg', 'jpeg')
+
+grey_image = image.convert('L')
+grey_image.save('images\\Grey.png', 'png')
+
+rotated_image = image.rotate(90)
+rotated_image.save('images\\Rotated.png', 'png')
+
+resized_image = image.resize((300, 300))
+resized_image.save('images\\Resized.png', 'png')
+
+box = (2433, 2061, 3449, 3061)
+cropped_image = image.crop(box)
+cropped_image.save('images\\Cropped.png', 'png')
+cropped_image.show()
