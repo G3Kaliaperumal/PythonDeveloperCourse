7f64de5a2b9f6f830ec5aeb5031515c439662e5f
G3Kaliaperumal <kaliaperumalgayathri27@gmail.com>
2020-06-23 21:40:15 +0530

    Lecture 219: Sending Emails With Python 3

--- Scripting With Python/sending_emails.py
+++ Scripting With Python/sending_emails.py
@@ -2,15 +2,21 @@
 # 1. https://docs.python.org/3/library/email.examples.html
 # 2. https://docs.python.org/3/library/email.html#module-email
 # 3. https://www.geeksforgeeks.org/simple-mail-transfer-protocol-smtp/
+# 4. https://treyhunner.com/2018/12/why-you-should-be-using-pathlib/
 
 import smtplib
 from email.message import EmailMessage
+from string import Template
+from pathlib import Path
+
+html = Template(Path('HtmlTemplate.html').read_text())
 
 email = EmailMessage()
 email['from'] = 'Gayathri Kaliaperumal'
 email['to'] = 'to_email@gmail.com'
 email['subject'] = 'Sending email via python'
-email.set_content('Yippee!! It worked')
+
+email.set_content(html.substitute({'name': 'Christopher'}), 'html')
 
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
     smtp.ehlo()
