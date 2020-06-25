9f0f40246e83fd244f3ec9a595b3ba8408346371
G3Kaliaperumal <kaliaperumalgayathri27@gmail.com>
2020-06-25 17:13:59 +0530

    Lecture 226: Password Checker 2

--- Scripting With Python/Password Checker/check_my_password.py
+++ Scripting With Python/Password Checker/check_my_password.py
@@ -12,12 +12,17 @@ def request_api_data(query_char):
     raise RuntimeError(f'Error fetching the response. Status: {res.status_code}. Please check the api and try again')
   return res
 
+def get_password_leaks_count(tail_hash, hashes):
+  hashes = (line.split(':') for line in hashes.splitlines())
+  for h, count in hashes:
+    if h == tail_hash:
+      return count
+
 
 def check_pwned_api(password):
   sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
-  result = request_api_data(sha1password[0:5])
-  print(result)
-
+  first5_chars, tail = sha1password[:5], sha1password[5:]
+  result = request_api_data(first5_chars)
+  print(get_password_leaks_count(tail, result.text))
 
 check_pwned_api('password123')
-
