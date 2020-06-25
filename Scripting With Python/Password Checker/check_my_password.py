d5a3253c832982b0ae5ab8e07fec2c1611cab905
G3Kaliaperumal <kaliaperumalgayathri27@gmail.com>
2020-06-25 16:56:50 +0530

    Lecture 225: Password Checker 1

--- Scripting With Python/Password Checker/check_my_password.py
+++ Scripting With Python/Password Checker/check_my_password.py
@@ -2,7 +2,22 @@
 # https://passwordsgenerator.net/sha1-hash-generator/
 
 import requests
+import hashlib
 
-url = 'https://api.pwnedpasswords.com/range/' + 'CBFDA'
-res = requests.get(url)
-print(res)
+
+def request_api_data(query_char):
+  url = 'https://api.pwnedpasswords.com/range/' + query_char
+  res = requests.get(url)
+  if res.status_code != 200:
+    raise RuntimeError(f'Error fetching the response. Status: {res.status_code}. Please check the api and try again')
+  return res
+
+
+def check_pwned_api(password):
+  sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+  result = request_api_data(sha1password[0:5])
+  print(result)
+
+
+check_pwned_api('password123')
+
