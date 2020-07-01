d9f64d8feeda7a2d45c578c57dc453f59e1348c5
G3Kaliaperumal <kaliaperumalgayathri27@gmail.com>
2020-07-02 00:13:55 +0530

    Lecture 244 - HackerNews Project

--- Scraping Data/HackerNewsProject.py
+++ Scraping Data/HackerNewsProject.py
@@ -5,4 +5,16 @@ res = requests.get('https://news.ycombinator.com/news')
 soup = BeautifulSoup(res.text, 'html.parser')
 links = soup.select('.storylink')
 votes = soup.select('.score')
-print(votes[0])
+
+def create_custom_hacker_news(links, votes):
+  hn = []
+  for index, link in enumerate(links):
+    points = int(votes[index].getText().replace(' points', ''))
+    print(points)
+    if points > 150:
+      title = link.getText()
+      href = link.get('href', None)
+      hn.append({'title': title, 'link': href})
+  return hn
+    
+print(create_custom_hacker_news(links, votes))
