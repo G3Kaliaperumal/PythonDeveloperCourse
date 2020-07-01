03356bda3f1e2f03a73b906dda92d9bd6da728ef
G3Kaliaperumal <kaliaperumalgayathri27@gmail.com>
2020-07-02 00:37:02 +0530

    Lecture 247 - HackerNews for more than a page

--- Scraping Data/HackerNewsProject.py
+++ Scraping Data/HackerNewsProject.py
@@ -3,9 +3,16 @@ from bs4 import BeautifulSoup
 from pprint import pprint
 
 res = requests.get('https://news.ycombinator.com/news')
+res2 = requests.get('https://news.ycombinator.com/news?p=2')
 soup = BeautifulSoup(res.text, 'html.parser')
+soup2 = BeautifulSoup(res2.text, 'html.parser')
 links = soup.select('.storylink')
+links2 = soup2.select('.storylink')
 subtext = soup.select('.subtext')
+subtext2 = soup2.select('.subtext')
+
+mega_links = links + links2
+mega_subtext = subtext + subtext2
 
 def sort_stories_by_points(hn_list):
   return sorted(hn_list, key = lambda k:k['points'], reverse = True)
@@ -22,4 +29,4 @@ def create_custom_hacker_news(links, subtext):
           hn.append({'title': title, 'link': href, 'points': points})
   return sort_stories_by_points(hn)
     
-pprint(create_custom_hacker_news(links, subtext))
+pprint(create_custom_hacker_news(mega_links, mega_subtext))
