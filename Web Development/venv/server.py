dd29673945c705b4cc06d2297b9dcb576975e50b
G3Kaliaperumal <kaliaperumalgayathri27@gmail.com>
2020-07-04 23:59:46 +0530

    Lecture 263  - Building a Portfolio 2

--- Web Development/venv/server.py
+++ Web Development/venv/server.py
@@ -9,5 +9,35 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def displayName():
+def default():
     return render_template('index.html')
+
+
+@app.route('/index.html')
+def renderIndexPage():
+    return render_template('index.html')
+
+
+@app.route('/work.html')
+def renderWorkPage():
+    return render_template('work.html')
+
+
+@app.route('/works.html')
+def renderWorksPage():
+    return render_template('works.html')
+
+
+@app.route('/about.html')
+def renderAboutPage():
+    return render_template('about.html')
+
+
+@app.route('/contact.html')
+def renderContactPage():
+    return render_template('contact.html')
+
+
+@app.route('/components.html')
+def renderComponentPage():
+    return render_template('components.html')
