7bb70892f3cc009934fcc263d8bb5faa780ae4b0
G3Kaliaperumal <kaliaperumalgayathri27@gmail.com>
2020-07-21 21:44:14 +0530

    Lecture 280 - Selenium Basics 2

--- Automation/automationViaSelenium.py
+++ Automation/automationViaSelenium.py
@@ -6,5 +6,13 @@ from selenium import webdriver
 chrome_browser = webdriver.Chrome('./chromedriver.exe')
 chrome_browser.maximize_window()
 chrome_browser.get('https://www.seleniumeasy.com/test/basic-first-form-demo.html')
-button = chrome_browser.find_element_by_class_name('btn-default')
-print(button.get_attribute('innerHTML'))
+
+user_message = chrome_browser.find_element_by_id('user-message')
+user_message.clear()
+user_message.send_keys('Hello!!')
+
+show_msg_button = chrome_browser.find_element_by_class_name('btn-default')
+show_msg_button.click()
+
+output_msg = chrome_browser.find_element_by_id('display')
+assert 'Hello!!' in output_msg.text
